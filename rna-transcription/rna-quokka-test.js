fbaabac02368cf34f5b2a99564779ca904359023
Clint Earl <clintsearl@gmail.com>
2019-03-18 11:35:33 -0600

    completed the RNA transcription

--- rna-transcription/rna-quokka-test.js
+++ rna-transcription/rna-quokka-test.js
@@ -1,32 +1,61 @@
+//Just brainstorming with quokka on this page.
+
+// let input = ["g"]
+
+// console.log(input)
 
 
-let input = ["g"]
 
-console.log(input)
+// const adjust = (input) =>{ input.map = input.toUpperCase()
+// // console.log(convert)
+
+// switch(input){
+//         case "A":
+//             input = "U"
+//         break
+//         case "T":
+//             input = "A"
+//         break
+//         case "C":
+//             input = "G"
+//         break
+//         case "G":
+//             input = "C"
+//         break
+//         case " ":
+//             input =" "
+//             break
+//         default:
+//             throw "Invalid Input DNA"
+//     }
+// }
+// console.log(adjust(" "))
+//  let i=0
+// do{
+//     let rnaString=""
+//         switch(dna){
+//                 case "A":
+//                     rnaString += "U"
+//                 break
+//                 case "T":
+//                     rnaString += "A"
+//                 break
+//                 case "C":
+//                     rnaString += "G"
+//                 break
+//                 case "G":
+//                     rnaString += "C"
+//                 break
+//                 case "":
+//                     rnaString = ""
+//                 break
+//                 default:
+//                     throw "Invalid DNA input"
+//         } while (i < 5);
+//         return rnaString
+//     }
 
 
 
-const adjust = (input) =>{ input.map = letter.toUpperCase()
-console.log(convert)}
-
-switch(convert){
-        case "A":
-            input = "U"
-        break
-        case "T":
-            input = "A"
-        break
-        case "C":
-            input = "G"
-        break
-        case "G":
-            input = "C"
-        break
-        default:
-            throw "Invalid Input DNA"
-    }
-
-console.log(convert)
 
 
-export {toRna}
--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,32 +1,35 @@
 
-export const toRna
-
-
- let transcriptor =   (toRna) => {
-        switch(toRna){
-            case "A":
-                toRna = "U"
-            break
-            case "T":
-                toRna = "A"
-            break
-            case "C":
-                toRna = "G"
-            break
-            case "G":
-                toRna = "C"
-            break
-            case '':
-                toRna = ''
-            break
-            default:
-                throw ''
+export const toRna = (dna) => {
+//Someone else used an array [] and .push to add them to the array, then .join [''] with '' to make it a string
+    let i = 0
+    let rnaString=""
+    // do{
+for(i=0; i< dna.length; i++){
+    //   had to make sure I was passing in i to iterate over the whole string, and look and each letter.
+        switch(dna.charAt(i)){
+                case "A":
+                    rnaString +="U"
+                break
+                case "T":
+                    rnaString  +="A"
+                break
+                case "C":
+                    rnaString  += "G"
+                break
+                case "G":
+                //
+                    rnaString  = rnaString.concat("C")
+                break
+                // don't seem to need this because it is declared as a "" and so would return that as well.... or maybe it doesn't care about the text in the default
+                // case "":
+                //     rnaString = ""
+                // break
+                default:
+                    throw "Invalid input DNA."
         }
-    
-    return toRna
-console.log(toRna)
+        // i++
+    // }while (i<= dna.length)  
     }
-
-transcriptor()
-
-
+   
+        return rnaString
+    }
